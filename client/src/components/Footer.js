645a39a623b2a4c5dc7ed895a0fa8748b43bed1b
SananM <smuzefferov1@gmail.com>
2023-08-02 14:57:29 +0400

    sneg

---
+++ client/src/components/Footer.js
@@ -0,0 +1,83 @@
+import React, { useState, useRef, useEffect } from "react";
+import flag from "../images/flag.png";
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBCollapse,
+} from "mdb-react-ui-kit";
+import { Link, useNavigate } from "react-router-dom";
+
+const GeneralNavbar = ({ proprole }) => {
+    const [showBasic, setShowBasic] = useState(false);
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        // You can store the dark mode preference in local storage or a state management system like Redux
+    };
+
+    const navigate = useNavigate();
+
+    return (
+        <MDBNavbar expand="lg" style={{ backgroundColor: "#fff", minHeight: "10vh" }}>
+            <MDBContainer fluid >
+                <MDBNavbarBrand>
+                    <span style={{ fontWeight: "bold", fontSize: "20px" }}>Made with ❤️ in <img src={flag} style={{ width: "30px" }} alt="flag" />
+                    </span>
+                </MDBNavbarBrand>
+                <MDBCollapse navbar show={showBasic} >
+                    <MDBNavbarNav className="mr-auto mb-2 mb-lg-0" style={{ width: "100%", display: "flex", justifyContent: "end" }}>
+                        <MDBNavbarItem>
+                            <MDBNavbarLink
+                                onClick={() => navigate(`/contactus`)}
+                                className="linkHover"
+                                style={{
+                                    fontWeight: "bold",
+                                    color: "#000",
+                                    cursor: "pointer",
+                                    fontSize: "20px"
+                                }}>
+                                Contact Us
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                        <MDBNavbarItem>
+                            <MDBNavbarLink
+                                onClick={() => navigate(`/terms`)}
+                                className="linkHover"
+                                style={{
+                                    fontWeight: "bold",
+                                    color: "#000",
+                                    cursor: "pointer",
+                                    fontSize: "20px"
+                                }}>
+                                Terms
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                        <MDBNavbarItem>
+                            <MDBNavbarLink
+                                onClick={() => navigate(`/privacypolicy`)}
+                                style={{
+                                    fontWeight: "bold",
+                                    color: "#000",
+                                    cursor: "pointer",
+                                    fontSize: "20px"
+                                }}
+                                className="linkHover"
+                                >
+                                Privacy Policy
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                    </MDBNavbarNav>
+                </MDBCollapse>
+            </MDBContainer>
+        </MDBNavbar>
+    );
+};
+export default GeneralNavbar;
---
+++ client/src/components/GeneralNavbar.js
@@ -0,0 +1,82 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import logo from "../images/logo.png";
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBCollapse,
+} from "mdb-react-ui-kit";
+import { Link, useNavigate } from "react-router-dom";
+
+const GeneralNavbar = ({ proprole }) => {
+    const [showBasic, setShowBasic] = useState(false);
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        // You can store the dark mode preference in local storage or a state management system like Redux
+    };
+
+    const navigate = useNavigate();
+
+    return (
+        <MDBNavbar expand="lg" dark>
+            <MDBContainer fluid>
+                <MDBContainer onClick={() => navigate(`/`)} fluid style={{display: "flex", width: "100%", alignItems: "center", justifyContent: "center"}}>
+                    <img style={{height: "50px", marginLeft: "250px"}} src={logo} alt="/"/>
+                </MDBContainer>
+            </MDBContainer>
+
+            <MDBCollapse navbar show={showBasic}>
+                    <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+                        <MDBNavbarItem style={{margin: "0px 5px"}}>
+                            <MDBNavbarLink
+                                onClick={() => navigate(`/auth/login`)}
+                                style={{
+                                    width: "100%",
+                                    backgroundColor: "#28a745",
+                                    borderColor: "#28a745",
+                                    marginLeft: "30%",
+                                    padding: "7px 35px",
+                                    borderRadius: "15px",
+                                    color: "#fff",
+                                    cursor: "pointer",
+                                    transition: "0.6s ease"
+                                }}
+                                className="logHover"
+                                >
+                                Login
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                        <MDBNavbarItem style={{margin: "0px 30px"}}>
+                            <MDBNavbarLink
+                                onClick={() => navigate(`/auth/register`)}
+                                style={{
+                                    width: "100%",
+                                    backgroundColor: "#4285F4",
+                                    borderColor: "#4285F4",
+                                    marginLeft: "25%",
+                                    padding: "7px 25px",
+                                    borderRadius: "15px",
+                                    color: "#fff",
+                                    cursor: "pointer",
+                                    transition: "0.6s ease"
+                                }}
+                                className="regHover"
+                                >
+                                Register
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                    </MDBNavbarNav>
+                </MDBCollapse>
+        </MDBNavbar>
+    );
+};
+export default GeneralNavbar;
